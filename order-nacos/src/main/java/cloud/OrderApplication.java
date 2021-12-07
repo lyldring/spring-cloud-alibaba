ac92273b4d3bc8f7fa1e96645e4bd0b63b0d2f63
Lyldrings <liyilin76582@163.com>
2021-12-07 22:04:50 +0800

    commit

---
+++ order-nacos/src/main/java/cloud/OrderApplication.java
@@ -0,0 +1,15 @@
+package cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liyl
+ * @create 2021-12-07 11:52
+ */
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class,args);
+    }
+}
---
+++ stock-nacos/src/main/java/com/cloud/StockApplication.java
@@ -0,0 +1,15 @@
+package com.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liyl
+ * @create 2021-12-07 11:52
+ */
+@SpringBootApplication
+public class StockApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StockApplication.class,args);
+    }
+}
